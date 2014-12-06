26e003247740c68d4606478a6d073ff4fa279b7c
Evan Saft <evan.saft@gmail.com>
2014-12-06 17:59:57 -0500

    Spawner stuff

--- Assets/WordPlatform.cs
+++ Assets/WordPlatform.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class WordPlatform : MonoBehaviour {
 
 	public Font textFont;
-	TextMesh tMesh;
-	BoxCollider2D bCollider;
+	private TextMesh tMesh;
+	private BoxCollider2D bCollider;
 
 	// Use this for initialization
 	void Start () {
-
+		InitializeText ("Test");
+		setPosition (7, 8);
 	}
 	
 	// Update is called once per frame
@@ -17,17 +18,18 @@ public class WordPlatform : MonoBehaviour {
 	
 	}
 
-	void InitializeText(string word){
+	public void InitializeText(string word){
 
 		tMesh = (TextMesh)this.gameObject.AddComponent ("TextMesh");
 		tMesh.font = textFont;
 		tMesh.text = word;
-		tMesh.characterSize = 1;
+		tMesh.characterSize = 1f;
 
 		tMesh.color = Color.black;
 
 		bCollider = (BoxCollider2D)this.gameObject.AddComponent ("BoxCollider2D");
-		this.gameObject.AddComponent("MeshRenderer");
+		bCollider.size = new Vector2 (bCollider.size.x, bCollider.size.y * .55f);
+		//this.gameObject.AddComponent("MeshRenderer");
 		gameObject.renderer.material = textFont.material;
 
 		Rigidbody2D rg = (Rigidbody2D)this.gameObject.AddComponent ("Rigidbody2D");
@@ -36,5 +38,11 @@ public class WordPlatform : MonoBehaviour {
 		rg.gravityScale = 0;
 		rg.fixedAngle = true;
 	}
+
+	public void setPosition(float x, float y){
+		this.transform.position = new Vector2 (x, y);
+		}
+
+
 }
 	                 

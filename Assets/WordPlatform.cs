38dfdabf8f0981af68680a7b06c0348b04076a6a
Evan Saft <evan.saft@gmail.com>
2014-12-05 23:28:31 -0500

    WordPlatform added

---
+++ Assets/WordPlatform.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WordPlatform : MonoBehaviour {
+
+	public Font textFont;
+	TextMesh tMesh;
+	BoxCollider2D bCollider;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void InitializeText(string word){
+
+		tMesh = (TextMesh)this.gameObject.AddComponent ("TextMesh");
+		tMesh.font = textFont;
+		tMesh.text = word;
+		tMesh.characterSize = 1;
+
+		tMesh.color = Color.black;
+
+		bCollider = (BoxCollider2D)this.gameObject.AddComponent ("BoxCollider2D");
+		this.gameObject.AddComponent("MeshRenderer");
+		gameObject.renderer.material = textFont.material;
+
+		Rigidbody2D rg = (Rigidbody2D)this.gameObject.AddComponent ("Rigidbody2D");
+
+		rg.isKinematic = true;
+		rg.gravityScale = 0;
+		rg.fixedAngle = true;
+	}
+}
+	                 

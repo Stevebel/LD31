95c083778abe7b2e1add39a125d982764d1c9de1
Steven B <stevebel6@gmail.com>
2014-12-07 14:23:48 -0500

    Added Level classes

---
+++ Assets/Level/Level.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Level {
+	private List<ObjectDefinition> definitions;
+
+	public Level(){
+		definitions = new List<ObjectDefinition>();
+
+		Add(new TextDefinition(0, "Adjust screen brightness until Viking is clearly visible").XPos(10).Color(1,1,1,1));
+	}
+	private void Add(ObjectDefinition o){
+		definitions.Add(o);
+	}
+}

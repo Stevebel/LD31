5de80a51dec9f56e88c9038ef1e2cc3e294d16c2
Steven B <stevebel6@gmail.com>
2014-12-06 20:05:35 -0500

    Hey look screen controls

---
+++ Assets/ScreenControls.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenControls : MonoBehaviour {
+	private EffectController effects;
+	// Use this for initialization
+	void Start () {
+		effects = GetComponent<EffectController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnButtonPressed(string button){
+		Debug.Log("Pressed " + button);
+		switch(button){
+		case "BrightnessUp":
+			effects.brightness += 0.1f;
+			break;
+		case "BrightnessDown":
+			effects.brightness -= 0.1f;
+			break;
+		}
+	}
+}

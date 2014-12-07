1391ee68a7fc17507e50ac7889f2542756455b38
Steven B <stevebel6@gmail.com>
2014-12-07 17:11:11 -0500

    Started level thing

--- Assets/Level/Level.cs
+++ Assets/Level/Level.cs
@@ -10,6 +10,9 @@ public class Level {
 
 		Add(new TextDefinition(0, "Adjust screen brightness until Viking is clearly visible").XPos(10).Color(1,1,1,1).Spaced(false));
 	}
+	public List<ObjectDefinition> getDefinitions(){
+		return definitions;
+	}
 	private void Add(ObjectDefinition o){
 		definitions.Add(o);
 	}
---
+++ Assets/Level/LevelController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelController : MonoBehaviour {
+	public Camera mainCamera;
+	private List<ObjectDefinition> definitions;
+	// Use this for initialization
+	void Start () {
+		definitions = new Level().getDefinitions();
+		definitions.Sort(delegate(ObjectDefinition a, ObjectDefinition b) {
+			return a.height.CompareTo(b.height);
+		});
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float maxHeight = mainCamera.transform.position.x + mainCamera.orthographicSize + 5f;
+
+		ObjectDefinition definition = definitions[0];
+		while(definition.height < maxHeight){
+			addDefinition(definition);
+			definitions.RemoveAt(0);
+			definition = definitions[0];
+		}
+	}
+
+	void addDefinition(ObjectDefinition definition){
+		//TODO
+	}
+}

12a8eeb007f77a34e91c00ece1eb448b19f79dce
Steven B <stevebel6@gmail.com>
2014-12-06 19:08:18 -0500

    Added overlay

---
+++ Assets/PushButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushButton : MonoBehaviour {
+	public Sprite upImage;
+	public Sprite downImage;
+	public string name = "Button";
+	public GameObject target;
+	private SpriteRenderer spriteRenderer;
+	private Collider2D collider2d;
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		collider2d = GetComponent<Collider2D>();
+		if(target == null){
+			target = gameObject;
+		}
+	}
+	
+	void OnMouseDown()
+	{
+		spriteRenderer.sprite = downImage;
+	}
+	
+	void OnMouseUp()
+	{
+		target.SendMessage("OnButtonPressed", name);
+		spriteRenderer.sprite = upImage;
+	}
+}

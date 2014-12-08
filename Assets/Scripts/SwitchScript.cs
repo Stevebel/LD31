2a682a378b1f2168f6d677b6fb3a06ff4d5e93c3
Evan Saft <evan.saft@gmail.com>
2014-12-07 19:30:15 -0500

    Switch works, therefore game is completed

---
+++ Assets/Scripts/SwitchScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchScript : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();	
+	}
+
+
+	void OnTriggerEnter2D(Collider2D c) {
+		if(c.gameObject.tag.Equals("Player")){
+			anim.SetBool("Flipped",true);
+		}
+	}
+
+}

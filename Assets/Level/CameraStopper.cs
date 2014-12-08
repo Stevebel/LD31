bfa6b15638f14eb6512086dce7ef9656ecd08c2c
apophis42 <apophis42@gmail.com>
2014-12-08 18:30:11 -0500

    Created real credits.

--- Assets/Level/CameraStopper.cs
+++ Assets/Level/CameraStopper.cs
@@ -4,9 +4,11 @@ using System.Collections;
 [RequireComponent(typeof(SpriteRenderer))]
 public class CameraStopper : MonoBehaviour
 {
+	[SerializeField] bool careAboutSwitch = false;
+
 	void OnBecameVisible()
 	{
-		if(!Switches.IsOn (0))
+		if(!Switches.IsOn (0) || !careAboutSwitch)
 			CameraController.instance.shouldScroll = false;
 	}
 }
